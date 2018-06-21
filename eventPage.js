52a1eda1b9cb64fc0f44884a8890acd2d84a08f5
Mike Veilleux <mveilleux@dyn.com>
2018-06-20 20:14:27 -0400

    Added ability to adjust ratings with user defined ratings.

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,5 @@
+var debug = true;
+
 // Enable Google analytics
 var _gaq = _gaq || [];
 !function() {
@@ -11,150 +13,177 @@ _gaq.push(["_setAccount", "UA-87086305-1"])
 
 
 // Setup alarm
-chrome.alarms.create("checkRun", {delayInMinutes: 1, periodInMinutes: 15} );
-chrome.alarms.onAlarm.addListener(function(alarm) {
+chrome.alarms.create("checkRun", {delayInMinutes: 1, periodInMinutes: 1} );
 
+// Alarm start
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (debug) console.log("Got alarm");
+  _gaq.push(["_trackEvent", "active", "ping"]);
+  getLocation();
+
+});
+
+// Message start
+chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
+{
   _gaq.push(["_trackEvent", "active", "ping"]);
 
+  if (debug) console.log("Got Message");
+  if( request.greeting === "NoDataOnLoad" )
+  {
+      if (debug) console.log("Message was: No data on load");
+      getLocation();
+      sendResponse( {message: "started"} );
+  }
+});
+
+function getLocation() {
+  if (debug) console.log("Started getLocation()");
   var options = {
     enableHighAccuracy: true,
     timeout: 10000,
     maximumAge: 0
   };
 
-  navigator.geolocation.getCurrentPosition(locationSuccess, error, options);
-});
+  // First, get the location, then get the weather
+  navigator.geolocation.getCurrentPosition(getWeather, error, options);
+}
 
-// Setup Listener from default_popup
-chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
-{
-  console.log("got request");
-  console.log(request);
-    if( request.greeting === "NoDataOnLoad" )
-    {
-        var options = {
-          enableHighAccuracy: true,
-          timeout: 10000,
-          maximumAge: 0
-        };
+function getWeather(position) {
 
-        navigator.geolocation.getCurrentPosition(locationSuccess, error, options);
-
-        sendResponse( {message: "started"} );
-    }
-});
-
-
-
-
-function locationSuccess(position) {
-
+  if (debug) console.log("Started getWeather()");
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
   var apikey = '098f9450e999345595390214cbf2ad3d';
   var url = "http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=" + lat + "&lon=" + lon + "&APPID=" + apikey;
 
-  $.getJSON(url, function(r){
+  $.getJSON(url, function(weather){
 
-    // ML Version
-    $.ajax({
-      type: "GET",
-      url: "train-data.csv",
-      dataType: "text",
-      success: function(response) {
-        data = $.csv.toArrays(response);
-        x = [];
-        y = [];
-        for (i = 0; i < data.length; ++i) {
-          tmp = data[i].splice(0,2);
-          x.push([parseFloat(tmp[0]), parseFloat(tmp[1])]);
-          y.push([parseFloat(data[i].splice(-1,1)[0])]);
-        }
-
-        mlr = new ML.MultivariateLinearRegression(x, y);
-        rating_prediction = (mlr.predict([r.main.temp, r.wind.speed])[0]).clamp(0,10);
-
-        console.log(rating_prediction);
-
-        set_new_rating(rating_prediction,
-                       {'temp': r.main.temp,
-                        'wind': r.wind.speed,
-                        'rating': rating,
-                        'position': position,
-                        'desc': r.weather[0].description}
-                      );
-      }
-    });
-
-    // Temp - r.main.temp
-    // Wind - r.wind.speed
-    // Weather Description - r.weather[0].description
-    rating = 10;
+    getUserMLData(weather);
 
     // Temp
-    x = r.main.temp;
-    console.log("Temp: " + r.main.temp + " degrees F");
-    switch (true) {
-      case (x >= 80):
-        rating -= 5;
-        break;
-      case (x > 55 && x < 80):
-        rating = rating;
-        break;
-      case (x > 40 && x <= 55):
-        rating -= 3;
-        break;
-      case (x <= 40):
-        rating -= 5;
-        break;
-    }
-
+    x = weather.main.temp;
     // Wind
-    x = r.wind.speed;
-    console.log("Wind: " + r.wind.speed + " mph");
-    switch (true) {
-      case (x >= 25):
-        rating -= 4;
-        break;
-      case (x > 15 && x < 25):
-        rating -= 2;
-        break;
-      case (x > 5 && x <= 15):
-        rating -= 1;
-        break;
-      case (x <= 5):
-        rating = rating;
-        break;
-    }
-
+    x = weather.wind.speed;
+    if (debug) console.log("Wind: " + weather.wind.speed + " mph");
+    // Weather Description
     // Check description eventually!
     // Documented here: http://openweathermap.org/weather-conditions
     // drizzle, rain, mist
-    console.log("Description is " + r.weather[0].description);
-    console.log("Description ID is " + r.weather[0].id);
-
-    // Rating
-    /* set_new_rating(rating, {'temp': r.main.temp,
-                            'wind': r.wind.speed,
-                            'rating': rating,
-                            'position': position,
-                            'desc': r.weather[0].description}
-                          ); */
+    if (debug) console.log("Description is " + weather.weather[0].description);
+    if (debug) console.log("Description ID is " + weather.weather[0].id);
 
   });
+}
+
+function getUserMLData(weather) {
+  if (debug) console.log("Started getUserMLData()");
+
+  chrome.storage.local.get(['userSetRatings'], function(mlData) {
+    mlData = mlData.userSetRatings;
+    if (debug) console.log(mlData);
+    if (mlData == null) {
+      mlData = {};
+    }
+    getStandardMLData(mlData, weather);
+  });
+
+}
+
+function getStandardMLData(mlUserData, weather) {
+  if (debug) console.log("Started getStandardMLData()");
+
+  // ML Version
+  $.ajax({
+    type: "GET",
+    url: "train-data.csv",
+    dataType: "text",
+    success: function(mlStandardData) {
+      mlStandardData = $.csv.toArrays(mlStandardData);
+      if (debug) console.log(mlStandardData);
+
+      // Iterate through standard data, adding each one to the user data array
+      for (var i = 0; i < mlStandardData.length; i++) {
+        if (mlUserData[mlStandardData[i][0]] == null) {
+          mlUserData[mlStandardData[i][0]] = {};
+          mlUserData[mlStandardData[i][0]][mlStandardData[i][1]] = mlStandardData[i][2];
+        } else {
+          if (mlUserData[mlStandardData[i][0]][mlStandardData[i][1]] == null) {
+            mlUserData[mlStandardData[i][0]][mlStandardData[i][1]] = mlStandardData[i][2];
+          } else {
+            console.log("Already set!");
+          }
+        }
+      }
+
+      // Check for a direct mapping
+      directRating = null;
+      if (typeof(mlUserData[weather.main.temp.toFixed(0)]) != 'undefined') {
+        if (typeof(mlUserData[weather.main.temp.toFixed(0)][weather.wind.speed.toFixed(0)]) != 'undefined') {
+          directRating = mlUserData[weather.main.temp.toFixed(0)][weather.wind.speed.toFixed(0)];
+        }
+      }
+
+      // Convert combined array back to array of arrays
+      var combinedData = [];
+      for (var temp in mlUserData) {
+        for (var wind in mlUserData[temp]) {
+          //console.log([temp, wind, mlUserData[temp][wind]]);
+          combinedData.push([temp, wind, mlUserData[temp][wind]]);
+        }
+      }
+      //combinedData = JSON.parse(JSON.stringify(combinedData))
+      if (debug) console.log(combinedData);
+      combinedData2 = JSON.parse(JSON.stringify(combinedData));
+      predictRating(combinedData2, weather, directRating);
+    }
+  });
+
+}
+
+function predictRating (mlData, weather, directRating) {
+  if (debug) console.log('Starting prediction rating');
+  if (debug) console.log(weather);
+  if (debug) console.log(mlData);
+
+  x = [];
+  y = [];
+  for (i = 0; i < mlData.length; ++i) {
+    // CSV has 3 columns (temp, wind speed, rating)
+    x.push([parseFloat(mlData[i][0]), parseFloat(mlData[i][1])]);
+    y.push([parseFloat(mlData[i][2])]);
+  }
+
+  mlr = new ML.MultivariateLinearRegression(x, y);
+  rating_prediction = (mlr.predict([weather.main.temp, weather.wind.speed])[0]).clamp(0,10);
+
+  if (directRating != null) {
+    rating_prediction = directRating;
+  }
+
+  if (debug) console.log(rating_prediction);
+
+
+  set_new_rating(rating_prediction,
+                 {'temp': weather.main.temp,
+                  'wind': weather.wind.speed,
+                  'rating': rating_prediction,
+                  'position': weather.coord,
+                  'desc': weather.weather[0].description}
+                );
 }
 
 function set_new_rating(rating, data) {
   console.log("Rating is " + rating);
 
   switch (true) {
-    case (x >= 7):
+    case (rating >= 7):
       chrome.browserAction.setIcon({path: "images/run_green_32.png"}); // Turn icon green
       break;
-    case (x > 4 && x < 7):
+    case (rating > 4 && rating < 7):
       chrome.browserAction.setIcon({path: "images/run_black_32.png"}); // Turn icon black
       break;
-    case (x <= 4):
+    case (rating <= 4):
       chrome.browserAction.setIcon({path: "images/run_red_32.png"}); // Turn icon red
       break;
   }
--- popup.js
+++ popup.js
@@ -19,10 +19,65 @@ var ICONS = {
   "broken clouds": "wi-day-sunny-overcast.svg",
   "shower rain": "wi-showers.svg",
   "rain": "wi-rain.svg",
+  "moderate rain": "wi-rain.svg",
   "thunderstorm": "wi-thunderstorm.svg",
   "snow": "wi-snow.svg",
   "mist": "wi-fog.svg",
 };
+
+var currentValues;
+var g;
+
+function getDashboardData(requestData = true) {
+  chrome.storage.local.get(['lastvalues'], function(data) {
+    currentValues = data;
+    console.log(data);
+    // This improves the user experience on initial load
+    if ($.isEmptyObject(data)) {
+      if (requestData) {
+        chrome.runtime.sendMessage({greeting: "NoDataOnLoad"}, function (response) {
+          console.log(response.message);
+        });
+      }
+      setTimeout(function(){
+        //location.reload();
+        getDashboardData(false);
+      }, 500);
+      return;
+    } else {
+      // Update values onto dashboard
+      updateDashboard(
+        data.lastvalues.rating,
+        data.lastvalues.temp.toFixed(0),
+        data.lastvalues.wind.toFixed(0),
+        data.lastvalues.desc);
+    }
+  });
+}
+
+function updateDashboard(rating, temp, wind, description) {
+
+  var weatherEl = $("#weather");
+  weatherEl.attr("src", getIcon(description));
+
+  $("#desc").html(description);
+  $("#temp").html(temp + "<span class='small'>&#8457;</span>");
+  $("#wind").html(wind + "<span class='small'>mph</span>");
+
+  g = new JustGage({
+    id: "gauge",
+    value: rating,
+    min: 0,
+    max: 10,
+    shadowOpacity: 0,
+    label: "Run Rating",
+    labelFontColor: "#4d4d4d",
+    valueFontColor: "#4d4d4d",
+    valueFontFamily: "Roboto",
+    hideMinMax: true,
+    levelColors: ["#c12b09", "#edd51e", "#1daa5a"]
+  });
+}
 
 // Get the correct weather icon based on the description
 // returned from the weather api. If we don't have an
@@ -34,14 +89,37 @@ function getIcon(description) {
   return ICON_PATH + DEFAULT_ICON;
 }
 
+function changeRating(val) {
+  chrome.storage.local.get(['userSetRatings'], function(data) {
+    data = data.userSetRatings;
+
+    if (data == null) {
+      data = {};
+    }
+    if (data[currentValues.lastvalues.temp.toFixed(0)] == null) {
+      data[currentValues.lastvalues.temp.toFixed(0)] = {};
+    }
+    updatedRating = (Math.round(currentValues.lastvalues.rating) + val).toString();
+    data[currentValues.lastvalues.temp.toFixed(0)][currentValues.lastvalues.wind.toFixed(0)] = updatedRating;
+
+    // Save it using the Chrome extension storage API.
+    chrome.storage.local.set({'userSetRatings': data}, function() {
+      console.log(data);
+      g.refresh(updatedRating);
+      currentValues.lastvalues.rating = updatedRating;
+      chrome.storage.local.set({'lastvalues': currentValues.lastvalues});
+    });
+  });
+}
+
 window.onload = function() {
 
-  $(".left-adj").click(function(){
-    alert('left');
+  $("#left-adj").click(function(){
+    changeRating(-1);
   });
 
-  $(".right-adj").click(function(){
-    alert('right');
+  $("#right-adj").click(function(){
+    changeRating(1);
   });
 
   $("#show-credits").click(function(){
@@ -54,51 +132,16 @@ window.onload = function() {
     $("#credits").hide();
   });
 
-  var weatherEl = $("#weather");
-
-  chrome.storage.local.get(['lastvalues'], function(data) {
-    //tmp = JSON.stringify(data);
-    if ($.isEmptyObject(data)) {
-      chrome.runtime.sendMessage({greeting: "NoDataOnLoad"}, function (response) {
-        console.log(response.message);
-      });
-
-      setTimeout(function(){
-        location.reload();
-      }, 10000);
-
-      return;
+  $( "#showcredits" ).click(function() {
+    if ($( "#showcredits" ).text() == 'Show credits') {
+      $(".footer-credits").css('display', 'block');
+      $( "#showcredits" ).text('Hide credits');
+    } else {
+      $(".footer-credits").css('display', 'none');
+      $( "#showcredits" ).text('Show credits');
     }
-
-    // set weather icon
-    weatherEl.attr("src", getIcon(data.lastvalues.desc));
-    $("#desc").html(data.lastvalues.desc);
-    $("#temp").html(data.lastvalues.temp.toFixed(0) + "<span class='small'>&#8457;</span>");
-    $("#wind").html(data.lastvalues.wind.toFixed(0) + "<span class='small'>mph</span>");
-
-    var g = new JustGage({
-      id: "gauge",
-      value: data.lastvalues.rating,
-      min: 0,
-      max: 10,
-      shadowOpacity: 0,
-      label: "Run Rating",
-      labelFontColor: "#4d4d4d",
-      valueFontColor: "#4d4d4d",
-      valueFontFamily: "Roboto",
-      hideMinMax: true,
-      levelColors: ["#c12b09", "#edd51e", "#1daa5a"]
-    });
-
-    $( "#showcredits" ).click(function() {
-      if ($( "#showcredits" ).text() == 'Show credits') {
-        $(".footer-credits").css('display', 'block');
-        $( "#showcredits" ).text('Hide credits');
-      } else {
-        $(".footer-credits").css('display', 'none');
-        $( "#showcredits" ).text('Show credits');
-      }
-    });
   });
+
+  getDashboardData();
 
 }
